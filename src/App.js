0d1636cb1ed2afe85824a10c901ab8a1b6339c3a
Abishek-05 <vpabishek5@gmail.com>
2023-05-18 19:13:02 +0530

    Included infinite scroll and loading bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-
 import React, { Component } from 'react'
 import Navbar from './components/Navbar'
 import News from './components/News';
@@ -8,25 +7,37 @@ import {
 	Routes,
 	Route
 } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
+
 export default class App extends Component {
-  render() {
-	return (
-		<div>
-			<Router>
-				<Navbar/>
-				<Routes>
-					{/* key parameter is to force remount component if a different category is chosen (video 31.) */}
-					<Route path = "/" element = {<News key = "general" pageSize={6} country = "in" category = "general"/>} />
-					<Route path = "/business" element = {<News key = "business" pageSize={6} country = "in" category = "business"/>} />
-					<Route path = "/entertainment" element = {<News key = "entertainment" pageSize={6} country = "in" category = "entertainment"/>} />
-					<Route path = "/health" element = {<News key = "health" pageSize={6} country = "in" category = "health"/>} />
-					<Route path = "/science" element = {<News key = "science" pageSize={6} country = "in" category = "science"/>} />
-					<Route path = "/sports" element = {<News key = "sports" pageSize={6} country = "in" category = "sports"/>} />
-					<Route path = "/technology" element = {<News key = "technology" pageSize={6} country = "in" category = "technology"/>} />
-				</Routes>
-			</Router>
-		</div>
-	)
-  }
+	//Moder way to define state without constructor
+	state = {
+		progress : 0
+	}
+
+	//Use arrow function or you need to bind function to this inside constructor
+	setProgress = (currProg) =>{
+		this.setState({progress : currProg})
+	}
+	render() {
+		return (
+			<div>
+				<Router>
+					<LoadingBar  height = {3} color='cyan' progress={this.state.progress} onLoaderFinished={() => this.setProgress(0)} />
+					<Navbar/>
+					<Routes>
+						{/* key parameter is to force remount component if a different category is chosen (video 31.) */}
+						<Route path = "/" element = {<News setProgress = {this.setProgress} key = "general" pageSize={6} country = "in" category = "general"/>} />
+						<Route path = "/business" element = {<News setProgress = {this.setProgress} key = "business" pageSize={6} country = "in" category = "business"/>} />
+						<Route path = "/entertainment" element = {<News setProgress = {this.setProgress} key = "entertainment" pageSize={6} country = "in" category = "entertainment"/>} />
+						<Route path = "/health" element = {<News setProgress = {this.setProgress} key = "health" pageSize={6} country = "in" category = "health"/>} />
+						<Route path = "/science" element = {<News setProgress = {this.setProgress} key = "science" pageSize={6} country = "in" category = "science"/>} />
+						<Route path = "/sports" element = {<News setProgress = {this.setProgress} key = "sports" pageSize={6} country = "in" category = "sports"/>} />
+						<Route path = "/technology" element = {<News setProgress = {this.setProgress} key = "technology" pageSize={6} country = "in" category = "technology"/>} />
+					</Routes>
+				</Router>
+			</div>
+		)
+  	}
 }
 
--- src/components/News.js
+++ src/components/News.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
-import Loader from './Loader';
+import Loader from './Loader'
 import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component'
 
 export default class News extends Component {
 
@@ -24,41 +25,39 @@ export default class News extends Component {
 		super();
 		this.state = {
 			articles : [],
-			loading : false,
-			page : 1
+			loading : true,
+			page : 1,
+			totalResults : 0
 		};
 
 		//To access the state objects in a user defined method, you can either bind the method to this explicitly in the constructor(as done here) or convert it into an arrow function
 		this.getNews = this.getNews.bind(this);
-		this.handleNextClick = this.handleNextClick.bind(this);
-		this.handlePrevClick = this.handlePrevClick.bind(this);
+		this.fetchMoreData = this.fetchMoreData.bind(this);
 	}
 	async getNews(offset)
 	{
-		this.setState({loading : true});
+		this.props.setProgress(10); //Loading bar
 		let url = `https://newsapi.org/v2/top-headlines?apiKey=04eaa632340142b4ae8e0081ce868ede&country=${this.props.country}&category=${this.props.category}&page=${this.state.page + offset}&pageSize=${this.props.pageSize}`;
 		let data = await fetch(url);
 		let parsedData = await data.json();
-		this.setState({	articles : parsedData.articles, 
+		this.setState({	articles : this.state.articles.concat(parsedData.articles), 
 						page : this.state.page + offset,
-						loading : false
+						loading : false,
+						totalResults : parsedData.totalResults
 					 });
+		this.props.setProgress(100);
 	}
-	async componentDidMount() //cdm runs after render() is called, so data is populated on 2nd render of page
+	//cdm runs after render() is called, so data is populated on 2nd render of page. It always runs twice in strict mode, comment it out to make it run only 
+	async componentDidMount()
 	{
 		this.getNews(0);
 	}
-	handleNextClick()
+	async fetchMoreData()
 	{
 		this.getNews(1);
 	}
-	handlePrevClick()
+	populateEle(element) //element describes each element of articles array
 	{
-		this.getNews(-1);
-	}
-	populateEle(element) 
-	{
-		//element describes each element of articles array
 		return (
 		//key should be unique for each element so we choose url as key
 		<div className="col-md-4" key={element.url}>
@@ -69,22 +68,21 @@ export default class News extends Component {
 
   render() { //We cannot modify states inside render(), it is a pure method
 	return (
-	  <div className="container my-4">
-		<h2>NewsMonkey- Top headlines!</h2>
+	<>
+		<h2 className="text-center my-3">NewsMonkey- Top headlines!</h2>
 		{this.state.loading && <Loader/>}
-
-		<div className="row">
-			{!this.state.loading &&
-				this.state.articles.map((element) => { return this.populateEle(element)} )
-				//For every element of articles array, call populateEle()
-				//By assigning this.articles to this.state.articles, the initial articles are made accessible within the component as this.state.articles. This allows the component to access and manipulate the articles using this.state.articles.
-  			}
-		</div>
-		<div className="container d-flex justify-content-between">
-			<button disabled = {this.state.page <=1} type="button" className="btn btn-dark mx-3" onClick={this.handlePrevClick}>&larr; Previous</button>
-			<button disabled = {this.state.articles.length === 0} type="button" className="btn btn-dark mx-3" onClick={this.handleNextClick} >Next &rarr;</button>
-		</div>
-	  </div>
+		<InfiniteScroll dataLength={this.state.articles.length} next={this.fetchMoreData} hasMore={this.state.articles.length !== this.state.totalResults} loader={<Loader/>} >
+			<div className="container my-4">
+				<div className="row">
+					{
+						this.state.articles.map((element) => { return this.populateEle(element)} )
+						//For every element of articles array, call populateEle()
+						//By assigning this.articles to this.state.articles, the initial articles are made accessible within the component as this.state.articles. This allows the component to access and manipulate the articles using this.state.articles.
+					}
+				</div>
+			</div>
+		</InfiniteScroll>
+	</>
 	)
   }
 }
